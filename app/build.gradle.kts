6bfb1dcb9501efb844f195272c11d1b098314e8f
nazliozbekk <nazliozbek14@gmail.com>
2024-11-21 16:22:58 +0300

    FireBase integrated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.storage.CacheResetOnProcessCanceled.enabled
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
 
 }
 
@@ -50,8 +51,11 @@ android {
         implementation(libs.androidx.activity)
         implementation(libs.androidx.constraintlayout)
         implementation(libs.androidx.navigation.fragment.ktx)
+        implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
+        implementation("com.google.firebase:firebase-analytics")
         implementation(libs.androidx.navigation.ui.ktx)
         testImplementation(libs.junit)
+        implementation(libs.firebase.analytics)
         androidTestImplementation(libs.androidx.junit)
         androidTestImplementation(libs.androidx.espresso.core)
         implementation("androidx.databinding:databinding-runtime:7.0.4")
